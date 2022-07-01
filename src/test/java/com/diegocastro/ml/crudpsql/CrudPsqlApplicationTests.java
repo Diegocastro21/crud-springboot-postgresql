5f4a46b630672d5f5495a046e30e022050678b94
Diegocastro21 <Diegocamilocastrooliveros@gmail.com>
2022-06-30 21:52:11 -0500

    Subida proyecto

---
+++ src/test/java/com/diegocastro/ml/crudpsql/CrudPsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegocastro.ml.crudpsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPsqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
