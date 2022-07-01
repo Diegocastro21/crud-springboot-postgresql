cbec2bd3a2921437d6f163d7c199e44f1e0f5ab0
Diegocastro21 <Diegocamilocastrooliveros@gmail.com>
2022-06-30 21:51:54 -0500

    Subida proyecto

---
+++ src/main/java/com/diegocastro/ml/crudpsql/CrudPsqlApplication.java
@@ -0,0 +1,13 @@
+package com.diegocastro.ml.crudpsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPsqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudPsqlApplication.class, args);
+    }
+
+}
